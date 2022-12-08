0b0d743283c72c99bd86abdb6f1c80a63c780216
MaBorz_05 <113910818+MaBorz@users.noreply.github.com>
2022-12-08 14:50:54 +0100

    run

--- Enzo 24/Assets/playermovement.cs
+++ Enzo 24/Assets/playermovement.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
+    
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
-
+    
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
